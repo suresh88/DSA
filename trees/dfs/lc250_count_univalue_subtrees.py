7a14e349cb2746368a6daea5c70aa98ea96fddf6
Suresh Kumar <suresh@Sureshs-MacBook-Air.local>
2022-05-14 00:18:36 -0700

    250. Count Univalue Subtrees

---
+++ trees/dfs/lc250_count_univalue_subtrees.py
@@ -0,0 +1,39 @@
+# 250. Count Univalue Subtrees
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        
+        def dfs(node):
+            # Base case: leaf node
+            if node.left is None and node.right is None:
+                return (1, True)
+            
+            # Recursive case: Internal node
+            amiunival = True
+            numunival = 0
+            if node.left is not None:
+                numleftunival, isleftunival = dfs(node.left)
+                if not isleftunival or node.val != node.left.val:
+                    amiunival = False
+                numunival += numleftunival
+                
+            if node.right is not None:
+                numrightunival, isrightunival = dfs(node.right)
+                if not isrightunival or node.val != node.right.val:
+                    amiunival = False
+                numunival += numrightunival
+            
+            if amiunival:
+                numunival += 1
+                
+            return numunival, amiunival            
+        (total, boolean) = dfs(root)
+        return total
