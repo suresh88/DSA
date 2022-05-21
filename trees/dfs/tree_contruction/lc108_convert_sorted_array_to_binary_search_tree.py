792e4cb7c46964d8a9d1121c5e5f1bd7e37eab19
Suresh Kumar <suresh@Sureshs-MacBook-Air.local>
2022-05-21 16:52:02 -0700

    Solved 108. Convert Sorted Array to Binary Search Tree using DFS

---
+++ trees/dfs/tree_contruction/lc108_convert_sorted_array_to_binary_search_tree.py
@@ -0,0 +1,25 @@
+# 108. Convert Sorted Array to Binary Search Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        def helper(A, start, end):
+            if start > end:
+                return None
+            elif start == end:
+                return TreeNode(A[start])
+            else:
+                mid = start + (end-start)//2
+                subtreeroot = TreeNode(A[mid])
+                subtreeroot.left = helper(A, start, mid-1)
+                subtreeroot.right = helper(A, mid+1, end)
+                
+                return subtreeroot
+            
+        return helper(nums, 0, len(nums)-1)
