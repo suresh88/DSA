441cb1625d8580b83174591f337c4a659ad33359
Suresh Kumar <suresh@Sureshs-MacBook-Air.local>
2022-05-20 22:32:22 -0700

    Solved 94. Binary Tree Inorder Traversal using DFS

---
+++ trees/dfs/lc94_binary_tree_inorder_traversal.py
@@ -0,0 +1,33 @@
+# 94. Binary Tree Inorder Traversal
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+        
+        result = []
+        stack = [(root, None)]
+        while len(stack) != 0:
+            (node, zone) = stack[-1]
+            if zone is None:
+                stack[-1] = (node, 'arrival')
+                if node.left is not None:
+                    stack.append((node.left, None))
+                    
+            elif zone == 'arrival':
+                stack[-1] = (node, 'interim')
+                result.append(node.val)
+                if node.right is not None:
+                    stack.append((node.right, None))
+                    
+            elif zone == 'interim':
+                stack[-1] = (node, 'departure')
+                stack.pop()
+                
+        return result
